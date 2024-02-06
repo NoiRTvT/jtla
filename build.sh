402459499911e39641baa049e46e4a5c60c686a9
NoiRTvT <NoiRTvT@gmail.com>
2024-02-06 15:40:09 +0300

    add build in one file

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 rm -rf ./dist
 rm -rf ./publish
 tsc --project ./tsconfig.json
+tsc --project ./tsconfig.json --outFile ./dist/jtla.js
 tsc-alias -p tsconfig.json
 mkdir publish
 cp -R ./dist ./publish
--- src/NArray/NArray.types.ts
+++ src/NArray/NArray.types.ts
@@ -1,20 +1,31 @@
-import {NArray} from "./index";
-import {NSet} from "@/NSet";
-import {NBy, NCollection} from "@/types";
+import { NArray } from "./index";
+import { NSet } from "@/NSet";
+import { NBy, NCollection } from "@/types";
 
 export interface NArrayType<T> extends NCollection<T> {
-    uniqBy<U>(by: NBy<T, U>): NArray<T>
+  uniqBy<U>(by: NBy<T, U>): NArray<T>;
 
-    toSet(): NSet<T, T>
+  toSet(): NSet<T, T>;
 
-    toSetBy<U>(by: NBy<T, U>): NSet<T, U>
+  toSetBy<U>(by: NBy<T, U>): NSet<T, U>;
 
-    // TODO: NOT TESTED
-    map<U>(callbackfn: (value: T, index: number, array: T[]) => U, thisArg?: any): NArray<U>;
-    filter(predicate: (value: T, index: number, array: T[]) => unknown, thisArg?: any): NArray<T>;
-    flatMap<U, This = undefined> (
-        callback: (this: This, value: T, index: number, array: T[]) => U | ReadonlyArray<U>,
-        thisArg?: This
-    ): NArray<U>
-    //
+  // TODO: NOT TESTED
+  map<U>(
+    callbackfn: (value: T, index: number, array: T[]) => U,
+    thisArg?: any,
+  ): NArray<U>;
+  filter(
+    predicate: (value: T, index: number, array: T[]) => unknown,
+    thisArg?: any,
+  ): NArray<T>;
+  flatMap<U, This = undefined>(
+    callback: (
+      this: This,
+      value: T,
+      index: number,
+      array: T[],
+    ) => U | ReadonlyArray<U>,
+    thisArg?: This,
+  ): NArray<U>;
+  //
 }
